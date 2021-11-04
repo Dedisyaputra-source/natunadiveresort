c0f9702537bf41c787ca20a6272332e944df7b59
Dedisyaputra-source <dedisyaputra611@gmail.com>
2021-11-04 08:11:44 +0700

    menambahkan header, halaman home, dan halaman about

--- natuna_dive_resort/resources/views/components/main.blade.php
+++ natuna_dive_resort/resources/views/components/main.blade.php
@@ -1,3 +1,29 @@
 <section class="home">
+    <div class="background-blur"></div>
     <img src="img/natuna-dive-resort-2.jpg" alt="" class="hero-image">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-hero">
+                    <h1>The First Ecofrendly Resort <br><span> In Natuna Island</span></h1>
+                    <small>book your privat tour across the island and explore underwater world with us</small>
+                    <button class="btn btn-primary">Book Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="about">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-6 about-img">
+                <img src="img/natuna-dive-resort-2.jpg" alt="" width="100">  
+            </div>
+            <div class="col-md-6 about-text">
+                <small><p>Welcome to Natuna Dive Resort</p></small>
+                <h1>Welcome To Our <br> Resort</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor eum maxime ad animi? Sint nihil dolorum sapiente harum doloribus vero odit eius animi quos deserunt aliquid consequatur laboriosam eum inventore quam cumque aliquam explicabo earum facere, accusantium hic assumenda dolorem aperiam! Excepturi laborum consectetur commodi asperiores ex, numquam quo soluta, reprehenderit doloribus tenetur voluptates, ea non alias impedit. Id cum veniam rem, eveniet hic ex quod delectus, obcaecati quae, praesentium accusantium dolores tempore enim eligendi eos quas doloribus dolore suscipit placeat. Assumenda tempore quidem totam, doloremque ratione corrupti iure quae hic? Provident incidunt tempore amet dolore atque quas rerum ea.</p>
+            </div>
+        </div>
+    </div>
 </section>
--- natuna_dive_resort/resources/views/components/navbar.blade.php
+++ natuna_dive_resort/resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-    <div class="container">
+    <div class="container-fluid">
       <a class="navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -16,10 +16,7 @@
             <a class="nav-link" href="#">Dive</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link">Rooms</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link btn-login">Login</a>
+            <a class="nav-link" href="#">Rooms</a>
           </li>
         </ul>
       </div>
