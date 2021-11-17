224e6b4ef50c5eb261a82a9b3b454629a5b7ec91
Dedisyaputra-source <dedisyaputra611@gmail.com>
2021-11-17 10:29:25 +0700

    membuat halaman admin

---
+++ natuna_dive_resort/resources/views/components/details_package.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Details Package</title>
+</head>
+<body>
+    <header>
+        <nav class="navigation">
+            <div class="logo"><img src="logo.png" alt=""></div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Package</a></li>
+                <li><a href="#">Dive site</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="details">
+        <img src="img-details.jpg" alt="">
+        <div class="text-details">
+            <small>Natuna Dive Resort</small>
+            <h1>Scuba And Stay</h1>
+            <p class="deskripsi">Dive and Stay at Natuna Dive Resort for 3 day 2 Night in a double or Twin Share Luxury room</p>
+            <p class="price">Rp. 3000.000</p>
+            <a href="#" class="btn-book">Booking</a>
+            <!-- <span class="booking-description">booking via whatsapps</span> -->
+        </div>
+    </div>
+    <div class="iternary">
+        <h3>Rencana Perjalan</h3>
+        <ol>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptate, molestiae sint, tempore quam recusandae, iste porro aliquam et molestias nihil. Nisi deserunt rerum iste voluptatibus accusamus, molestiae quidem officia?</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptate, molestiae sint, tempore quam recusandae, iste porro aliquam et molestias nihil. Nisi deserunt rerum iste voluptatibus accusamus, molestiae quidem officia?</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptate, molestiae sint, tempore quam recusandae, iste porro aliquam et molestias nihil. Nisi deserunt rerum iste voluptatibus accusamus, molestiae quidem officia?</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptate, molestiae sint, tempore quam recusandae, iste porro aliquam et molestias nihil. Nisi deserunt rerum iste voluptatibus accusamus, molestiae quidem officia?</li>
+            <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptate, molestiae sint, tempore quam recusandae, iste porro aliquam et molestias nihil. Nisi deserunt rerum iste voluptatibus accusamus, molestiae quidem officia?</li>
+        </ol>
+    </div>
+</body>
+</html>
